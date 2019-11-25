30f8f2d8e3f4b2a23427d8d51238b81d46d5be51
Aurora <625716987@qq.com>
2019-11-25 21:02:15 +0800

    fds

---
+++ 课堂笔记/test.js
@@ -0,0 +1,26 @@
+
+var obj = {
+    fn_test:function(){
+    },
+    init: function () {
+        this.fn_test();
+    }
+}
+$(function () {
+    obj.init()
+})
+
+
+
+var obj = {
+    fn_test:function(){
+    },
+    init: function () {
+        this.fn_test();
+    }
+}
+$(function () {
+    obj.init()
+})      
+
+
